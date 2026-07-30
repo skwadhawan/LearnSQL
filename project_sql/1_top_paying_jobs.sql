198f21aa2cb53c9f157d53f9de652dbe7ababacd
skwadhawan <sambhavacademic@gmail.com>
2026-07-31 02:13:27 +0530

    first commit

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,7 @@
+/*
+Question: What are the top paying data analyst jobs?
+- first i will identify the top 10 highest-paying data analyst roles that are available remotely.
+- then i will focus on job postings with specified salaries (will ignore nulls)
+- why am i doing this? To highlight the top-paying opportunities for data analysts, offering insigts into employment options and location flexibility.
+*/
+
